0d4db3ee11d3668f18861fd1b0d47a5d5a3806d6
Ocraftyone <Ocraftyone@users.noreply.github.com>
2022-06-03 17:21:17 -0400

    begin working on corn crop

--- src/main/java/com/ocraftyone/randomadditions/blocks/CornCrop.java
+++ src/main/java/com/ocraftyone/randomadditions/blocks/CornCrop.java
@@ -1,18 +1,25 @@
 package com.ocraftyone.randomadditions.blocks;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
 public class CornCrop extends CropBlock  {
     private final Supplier<? extends ItemLike> seedItem;
+    public static final BooleanProperty UPPER = BooleanProperty.create("upper");
     
     public CornCrop(Properties properties, Supplier<? extends ItemLike> seed) {
         super(properties);
         this.seedItem = seed;
-        
+        this.registerDefaultState(this.getStateDefinition().any().setValue(AGE, 0).setValue(UPPER, false));
     }
     
     @Override
@@ -20,4 +27,22 @@ public class CornCrop extends CropBlock  {
         return this.seedItem.get();
     }
     
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(AGE, UPPER);
+    }
+    
+    public BooleanProperty getUpperProperty() {
+        return UPPER;
+    }
+    
+    @Override
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        BlockPos down = pPos.below();
+        if (pLevel.getBlockState(down).is(this)) {
+            return !pLevel.getBlockState(down).getValue(this.getUpperProperty())
+                    && super.canSurvive(pState, pLevel, pPos);
+        }
+        return super.canSurvive(pState, pLevel, pPos);
+    }
 }
